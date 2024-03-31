7273d16023b27d1ac03555bc8ee76e02f6d6a07a
AmaniSiwarProject <161306133+AmaniSiwarProject@users.noreply.github.com>
2024-03-31 05:50:14 +0200

    Update Event1.java

--- src/main/java/Wedding/Planner/Event1.java
+++ src/main/java/Wedding/Planner/Event1.java
@@ -1,35 +1,40 @@
 package Wedding.Planner;
-import java.util.logging.Logger;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Event1 {
-	
-	  private String name;
-	    private LocalDate date;
-	    private List<Task> tasks;
-    private static final Logger logger = Logger.getLogger(Event1.class.getName()); 
 
-	    public Event1(String name, LocalDate date) {
-	        this.name = name;
-	        this.date = date;
-	        this.tasks = new ArrayList<>();
-	    }
+    private static final Logger logger = Logger.getLogger(Event1.class.getName());
+    
+    private String name;
+    private LocalDate date;
+    private List<Task> tasks;
 
-	    public void addTask(Task task) {
-	        tasks.add(task);
-	    }
+    public Event1(String name, LocalDate date) {
+        this.name = name;
+        this.date = date;
+        this.tasks = new ArrayList<>();
+    }
 
-	    public List<Task> getTasks() {
-	        return tasks;
-	    }
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
 
-	    public void displayEventDetails() {
-	        logger.info("Event: " + name + " on " + date);
-	        for (Task task : tasks) {
-	            logger.info("   " + task);
-	        }
-	    }
-	    
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void displayEventDetails() {
+        if (logger.isLoggable(Level.INFO)) {
+            logger.info(String.format("Event: %s on %s", name, date));
+            for (Task task : tasks) {
+                logger.info("   " + task);
+            }
+        }
+    }
+    
 }
