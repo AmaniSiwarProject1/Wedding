536aea7a99aab4dba67dbf211dfa68ecf131ff2b
AmaniSiwarProject <161306133+AmaniSiwarProject@users.noreply.github.com>
2024-04-01 14:27:45 +0200

    Update AddEventTest.java

--- src/test/java/AddEventTest.java
+++ src/test/java/AddEventTest.java
@@ -1,5 +1,5 @@
-import wedding.planner.AddEvent;
-import wedding.planner.EventManagment;
+import Wedding.Planner.AddEvent;
+import Wedding.Planner.EventManagment;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
